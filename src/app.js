bc058c7272f2788534b80e53fe752a8fc51af958
anajbsouza <anaju.batsouza@gmail.com>
2023-07-03 00:25:39 -0300

    messages ok

--- src/app.js
+++ src/app.js
@@ -28,6 +28,13 @@ const db = mongoClient.db()
 // schemas
 const schemaParticipant = joi.object({ name: joi.string().required()});
 
+const schemaMessage = joi.object({
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.string().valid('message', 'private_message').required()
+});
+
+
 // funções
 app.post("/participants", async(req, res) => {
     const { name } = req.body;
@@ -67,11 +74,67 @@ app.get("/participants", async(req, res) => {
 })
 
 app.post("/messages", async(req, res) => {
+    const validation = schemaMessage.validate(req.body, { abortEarly: false });
+
+    if (validation.error) {
+        const errors = validation.error.details.map(detail => detail.message);
+        return res.status(422).send(errors);
+    }
+
+    const from = req.headers.user;
+    if(!from) {
+        return res.status(422).send("Header 'User' é obrigatório");
+    }
+
+    const user = await db.collection("participants").findOne({ name: from });
+    if(!user) {
+        return res.status(422).send('Participante não está na sala');
+    }
+
+    const { to, text, type } = req.body;
+
+    const time = dayjs().format('HH:mm:ss');
+    await db.collection("messages").insertOne({ from, to, text, type, time });
+
+    return res.status(201).send();
 
 })
 
 app.get("/messages", async(req, res) => {
+    try {
+        const user = req.headers.user;
+        if(!user) {
+            return res.status(422).send("Header 'User' é obrigatório");
+        }
 
+        const limit = req.query.limit;
+        if(limit !== undefined) {
+            const limitNumber = Number(limit);
+            if(!Number.isInteger(limitNumber) || limitNumber <= 0) {
+                return res.status(422).send("Limit inválido");
+            }
+        }
+
+        const messagesQuery = db.collection("messages").find({
+            $or: [
+                { to: user },
+                { from: user },
+                { type: "message" },
+                { to: "Todos" }
+            ]
+        });
+
+        let messages;
+        if(limit !== undefined) {
+            messages = await messagesQuery.sort({ time: -1 }).limit(Number(limit)).toArray();
+        } else {
+            messages = await messagesQuery.toArray();
+        }
+
+        res.send(messages);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
 })
 
 app.post("/status", async(req, res) => {
@@ -79,5 +142,5 @@ app.post("/status", async(req, res) => {
 })
 
 // deixa o app escutando
-const PORT = 5000;
+const PORT = 5008;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
